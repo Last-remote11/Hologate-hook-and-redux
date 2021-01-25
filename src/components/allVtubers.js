8063b9bfed1dba8f8da163f4217fb09dcc2513ec
Sung Dong Kim <67620856+Last-remote11@users.noreply.github.com>
2021-01-25 16:26:04 +0900

    apply hooks

--- src/components/allVtubers.js
+++ src/components/allVtubers.js
@@ -1,4 +1,4 @@
-export const Vtubers =[
+export const allVtubers =[
     {
         id: 1,
         name: 'Usada Pekora',
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
-import React, { StrictMode } from 'react';
-import { Vtubers } from '../components/Vtubers';
+import React, { StrictMode, useState, useEffect } from 'react';
+import { allVtubers } from '../components/allVtubers';
 import CardList from './CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -12,56 +12,69 @@ import {Helmet} from 'react-helmet';
 
 
 
-class App extends React.Component {
+const App = () => {
 
-    constructor() {
-        super();
-        this.state = {
-            Vtubers: Vtubers,
-            searchfield: '',
-            background: 'body { background-color: black; }',
-        };
-    };
+    // useState(초기값)는 state와 state를 변경시키는 함수를 반환 
+    const [Vtubers, setVtubers] = useState(allVtubers)
+    const [searchfield, setSearchfield] = useState('')
+    const [background, setBackground] = useState('body { background-color: black; }')
 
-    
-    enableDarkMode = () => {
-        this.state.background.includes('white') ?
-            this.setState( {background: 'body { background-color: black; }'} ) :
-            this.setState( {background: 'body { background-color: white; }'} )
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         Vtubers: Vtubers,
+    //         searchfield: '',
+    //         background: 'body { background-color: black; }',
+    //     };
+    // };
+
+    // 만약 componentDidMount()를 썼다면 useEffect로 대체
+    // 두번째 파라미터에서는 배열 안의 값이 바뀔때만 첫번째 함수를 실행하라는뜻
+    // 만약 아무것도 넣지 않으면 무한으로 첫번째 파라미터(함수)를 실행
+    // 빈 배열을 넣으면 처음 시작할때만 실행
+    // useEffect(() => {
+    //     fetch('https://blarblar.com')
+    //     .then(response => response.json())
+    //     .then(data => setVtubers(data))
+    // }, [])
+
+    const enableDarkMode = () => {
+        background.includes('white') ?
+            setBackground('body { background-color: black; }') :
+            setBackground('body { background-color: white; }')
         }
 
-    onSearch = (event) => {
-        this.setState( {searchfield: event.target.value});
+    const onSearch = (event) => {
+        setSearchfield(event.target.value);
     };
 
-    render() {
-        const filteredVtubers = this.state.Vtubers.filter(Vtubers => {
-        return Vtubers.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-            });
-        
-        if (!this.state.Vtubers) {
-            return <h1>loading...</h1>
-        } else {
-        return (
-            <StrictMode>
-                <div className='tc'>
-                    <Helmet>
-                        <style>{this.state.background}</style>
-                    </Helmet>
-                    <title>Hologate</title>
-                    <Darkmode enableDarkMode={this.enableDarkMode}/>
-                    <h1 className='f1'>Hologate</h1>
-                    <SearchBox searchChange={this.onSearch} />
-                        <Sidebar/>   
-                    {/* <Scroll> */}
-                        <ErrorBoundary>
-                        <CardList Vtubers={ filteredVtubers }/>
-                        </ErrorBoundary>
-                    {/* </Scroll> */}
-                </div>
-            </StrictMode>
-            )};
-    }
+
+    const filteredVtubers = Vtubers.filter(Vtubers => {
+    return Vtubers.name.toLowerCase().includes(searchfield.toLowerCase())
+        });
+    
+    if (!Vtubers) {
+        return <h1>loading...</h1>
+    } else {
+    return (
+        <StrictMode>
+            <div className='tc'>
+                <Helmet>
+                    <style>{background}</style>
+                </Helmet>
+                <title>Hologate</title>
+                <Darkmode enableDarkMode={enableDarkMode}/>
+                <h1 className='f1'>Hologate</h1>
+                <SearchBox searchChange={onSearch} />
+                    <Sidebar/>   
+                {/* <Scroll> */}
+                    <ErrorBoundary>
+                    <CardList Vtubers={ filteredVtubers }/>
+                    </ErrorBoundary>
+                {/* </Scroll> */}
+            </div>
+        </StrictMode>
+        )};
 };
 
 export default App;
